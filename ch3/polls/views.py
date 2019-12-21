fa44c8ce202ad670df9dd6dab3333365bae30628
dpfprtus <dpfprtus@gmail.com>
2019-12-21 20:35:58 +0900

    Add scanner site (error)

--- ch3/polls/views.py
+++ ch3/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'polls/index.html')
+def index2(request):
+    return render(request, 'polls/index2.html')
